b10d2ff4f9d04710ab435aa188faaa19b58470cd
_DUNDUN <1053450626@qq.com>
2022-09-20 09:45:08 +0800

    python基础

---
+++ 入门知识点/字面量.py
@@ -0,0 +1,5 @@
+print(666)
+print(13.14)
+print("caonima")
+
+# 被写在代码中的固定值就是字面量
---
+++ 入门知识点/数据类型.py
@@ -0,0 +1,9 @@
+
+# type()查看数据类型，参数可以是字面量或变量
+print(type("caonima"))
+# type() 返回值是类型，可以赋值给变量
+float_type = type(29.92)
+print(float_type)
+# type()查看变量类型
+name = "nimalegechoubi"
+print(type(name))
