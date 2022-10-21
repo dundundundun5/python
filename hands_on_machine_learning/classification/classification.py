8bad9b0b59f06391c0e80e4eb1d9d31d4bce795e
_DUNDUN <1053450626@qq.com>
2022-10-21 10:04:01 +0800

    实验：分类问题

--- hands_on_machine_learning/classification/classification.py
+++ hands_on_machine_learning/classification/classification.py
@@ -35,5 +35,89 @@ def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
     if tight_layout:
         plt.tight_layout()
     plt.savefig(path, format=fig_extension, dpi=resolution)
-
+# ========================================================
 from sklearn.datasets import fetch_openml
+mnist = fetch_openml("mnist_784", version=1, as_frame=False)
+
+print(mnist.keys())
+X, y = mnist["data"], mnist["target"]
+print(X.shape)
+print(y.shape)
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.use("TkAgg")
+
+some_digit = X[0]
+some_digit_image = some_digit.reshape(28, 28)
+plt.imshow(some_digit_image, cmap=mpl.cm.binary)
+plt.axis("off")
+plt.show()
+
+y = y.astype(np.uint8)
+
+
+def plot_digit(data):
+    image = data.reshape(28, 28)
+    plt.show(image, cmap=mpl.cm.binary, interpolation="nearest")
+    plt.axis("off")
+
+
+def plot_digits(instances, images_per_row=10, **options):
+    size = 28
+    images_per_row = min(len(instances), images_per_row)
+    # This is equivalent to n_rows = ceil(len(instances) / images_per_row):
+    n_rows = (len(instances) - 1) // images_per_row + 1
+
+    # Append empty images to fill the end of the grid, if needed:
+    n_empty = n_rows * images_per_row - len(instances)
+    padded_instances = np.concatenate([instances, np.zeros((n_empty, size * size))], axis=0)
+
+    # Reshape the array so it's organized as a grid containing 28×28 images:
+    image_grid = padded_instances.reshape((n_rows, images_per_row, size, size))
+
+    # Combine axes 0 and 2 (vertical image grid axis, and vertical image axis),
+    # and axes 1 and 3 (horizontal axes). We first need to move the axes that we
+    # want to combine next to each other, using transpose(), and only then we
+    # can reshape:
+    big_image = image_grid.transpose(0, 2, 1, 3).reshape(n_rows * size, images_per_row * size)
+    # Now that we have a big image, we just need to show it:
+    plt.imshow(big_image, cmap=mpl.cm.binary, **options)
+    plt.axis("off")
+
+
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+y_train_5 = (y_train == 5)
+y_test_5 = (y_test == 5)
+
+from sklearn.linear_model import SGDClassifier
+sgd_clf =  SGDClassifier(max_iter=1000, tol=1e-3, random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+sgd_clf.predict([some_digit])
+
+from sklearn.model_selection import cross_val_score
+cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring="accuracy")
+
+from sklearn.model_selection import StratifiedKFold
+from sklearn.base import clone
+skfolds = StratifiedKFold(n_splits=3, shuffle=True, random_state=42)
+for train_index, test_index in skfolds.split(X_train, y_train_5):
+    clone_clf = clone(sgd_clf)
+    X_train_folds = X_train[train_index]
+    y_train_folds = y_train_5[train_index]
+    X_test_fold = X_train[test_index]
+    y_test_fold = y_train_5[test_index]
+
+    clone_clf.fit(X_train_folds, y_train_folds)
+    y_pred = clone_clf.predict(X_test_fold)
+    n_correct = sum(y_pred == y_test_fold)
+    print(n_correct / len(y_pred))
+
+from sklearn.base import BaseEstimator
+class Never5Classfier(BaseEstimator):
+    def fit(self,X, y=None):
+        pass
+    def predict(self, X):
+        return np.zeros((len(X),1), dtype=bool)
+b
