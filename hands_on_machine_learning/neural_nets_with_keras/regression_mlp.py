9efdec6d871fa8eb96a45b21066fd864746cf064
dundundundun5 <1053450626@qq.com>
2022-11-08 16:28:42 +0800

    回归mlp，完结

--- hands_on_machine_learning/neural_nets_with_keras/regression_mlp.py
+++ hands_on_machine_learning/neural_nets_with_keras/regression_mlp.py
@@ -1,9 +1,12 @@
+import numpy as np
 import pandas as pd
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-import tensorflow as tf
 from tensorflow import keras
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 housing = fetch_california_housing()
 #
 X_train_full, X_test, y_train_full, y_test = train_test_split(
@@ -17,8 +20,7 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_valid = scaler.transform(X_valid)
 X_test = scaler.transform(X_test)
-
-#
+"""
 model = keras.models.Sequential([
     keras.layers.Dense(30, activation='relu', input_shape=X_train.shape[1:]),
     keras.layers.Dense(1)
@@ -30,6 +32,10 @@ history = model.fit(X_train, y_train, epochs=20, validation_data=(X_valid, y_val
 mse_test = model.evaluate(X_test, y_test)
 X_new = X_test[:3]
 y_pred = model.predict(X_new)
+"""
+X_new = X_test[:3]
+
+#
 
 #
 """
@@ -42,9 +48,9 @@ plt.gca().set_ylim(0, 1)
 plt.show()
 """
 
-
 # ====================================================
 # 函数式API
+"""
 input_ = keras.layers.Input(shape=X_train.shape[1:])
 hidden1 = keras.layers.Dense(30, activation="relu")(input_)
 hidden2 = keras.layers.Dense(30, activation='relu')(hidden1)
@@ -53,8 +59,10 @@ output = keras.layers.Dense(1)(concat)
 model = keras.models.Model(inputs=[input_], output=[output])
 model.summary()
 
+"""
+
 # 宽路径送入特征的子集A，深路径送入特征的子集B
-input_A = keras.layers.Input(shape=[5], name='wide_input')
+"""input_A = keras.layers.Input(shape=[5], name='wide_input')
 input_B = keras.layers.Input(shape=[6], name='deep_input')
 hidden1 = keras.layers.Dense(30, activation='relu')(input_B)
 hidden2 = keras.layers.Dense(30, activation='relu')(hidden1)
@@ -73,7 +81,10 @@ history = model.fit((X_train_A, X_train_B), y_train, epochs=20,
                     validation_data=((X_valid_A, X_valid_B), y_valid))
 mse_test = model.evaluate((X_test_A, X_test_B), y_test)
 y_pred = model.predict((X_new_A, X_new_B))
+"""
+
 # ========================================
+"""
 input_A = keras.layers.Input(shape=[5], name="wide_input")
 input_B = keras.layers.Input(shape=[6], name="deep_input")
 hidden1 = keras.layers.Dense(30, activation="relu")(input_B)
@@ -93,9 +104,11 @@ history = model.fit([X_train_A, X_train_B], [y_train, y_train], epochs=20,
 total_loss, main_loss, aux_loss = model.evaluate(
     [X_test_A, X_test_B], [y_test, y_test]
 )
+"""
+
 # =======================================
 
-
+"""
 class WideAndDeepModel(keras.Model):
     def __init__(self, units=30, activation="relu", **kwargs):
         super().__init__(**kwargs)
@@ -120,35 +133,45 @@ history = model.fit((X_train_A, X_train_B),(y_train, y_train), epochs=10,
                     validation_data=((X_valid_A, X_valid_B), (y_valid, y_valid)))
 total_loss, main_loss, aux_loss = model.evaluate((X_test_A, X_test_B), (y_test, y_test))
 y_pred_main, y_pred_aux = model.predict((X_new_A, X_new_B))
-#
+# 保存模型
 model.save("my_keras_model.h5")
 model.save_weights("my_keras_weights.ckpt")
+model = keras.load_model('my_keras_model.h5')
+model.load_weight("my_keras_weights.ckpt")
+"""
 
-#
-model = keras.models.Sequential([
-    keras.layers.Dense(30, activation="relu", input_shape=[8]),
-    keras.layers.Dense(30, activation="relu"),
-    keras.layers.Dense(1)
-])
-model.compile(loss="mse", optimizer=keras.optimizers.SGD(learning_rate=1e-3))
+# ====================================================
+# 普通回调
+# model = keras.models.Sequential([
+#     keras.layers.Dense(30, activation="relu", input_shape=[8]),
+#     keras.layers.Dense(30, activation="relu"),
+#     keras.layers.Dense(1)
+# ])
+# model.compile(loss="mse", optimizer=keras.optimizers.SGD(learning_rate=1e-3))
 checkpoint_cb = keras.callbacks.ModelCheckpoint("my_keras_model.h5", save_best_only=True)
-history = model.fit(X_train, y_train, epochs=10,
-                    validation_data=(X_valid, y_valid),
-                    callbacks=[checkpoint_cb])
-model = keras.models.load_model("my_keras_model.h5") # rollback to best model
-mse_test = model.evaluate(X_test, y_test)
+# history = model.fit(X_train, y_train, epochs=10,
+#                     validation_data=(X_valid, y_valid),
+#                     callbacks=[checkpoint_cb])
+# model = keras.models.load_model("my_keras_model.h5") # rollback to best model
+# mse_test = model.evaluate(X_test, y_test)
 
-#
+
+# ===================================================
+# 提早停止回调
+"""
 model.compile(loss="mse", optimizer=keras.optimizers.SGD(learning_rate=1e-3))
 early_stopping_cb = keras.callbacks.EarlyStopping(patience=10,
                                                   restore_best_weights=True)
-history = model.fit(X_train, y_train, epochs=10,
+history = model.fit(X_train, y_train, epochs=100,
                     validation_data=(X_valid, y_valid),
                     callbacks=[checkpoint_cb])
 model = keras.models.load_model("my_keras_model.h5") # rollback to best model
 mse_test = model.evaluate(X_test, y_test)
+"""
+# ===================================================
 
-#
+# 自定义回调
+"""
 class PrintValTrainRatioCallback(keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
         print("\nval/train: {:.2f}".format(logs["val_loss"] / logs["loss"]))
@@ -156,3 +179,81 @@ val_train_ratio_cb = PrintValTrainRatioCallback()
 history = model.fit(X_train, y_train, epochs=1,
                     validation_data=(X_valid, y_valid),
                     callbacks=[val_train_ratio_cb])
+"""
+
+# =====================================================
+# 用于tensorboard日志的根日志目录+根据当前日期和时间生成一个子目录
+import os
+
+root_logdir = os.path.join(os.curdir, "my_logs")
+
+
+def get_run_logdir():
+    import time
+    run_id = time.strftime('run_%Y_%m_%d-%H_%M_%S')
+    return os.path.join(root_logdir, run_id)
+
+
+# run_logdir = get_run_logdir()
+# model = keras.models.Sequential([
+#     keras.layers.Dense(30, activation='relu', input_shape=[8]),
+#     keras.layers.Dense(30, activation='relu'),
+#     keras.layers.Dense(1)
+# ])
+# model.compile(loss='mse', optimizer=keras.optimizers.SGD(learning_rate=1e-3))
+# tensorboard_cb = keras.callbacks.TensorBoard(run_logdir)
+# history = model.fit(X_train, y_train, epochs=30,
+#                     validation_data=(X_valid, y_valid),
+#                     callbacks=[checkpoint_cb, tensorboard_cb])
+# ===============================================
+# 创建SummaryWriter，记录标量、直方图、图像、音频、文本
+import tensorflow as tf
+test_logdir = get_run_logdir()
+writer = tf.summary.create_file_writer(test_logdir)
+with writer.as_default():
+    for step in range(1, 1000 + 1):
+        tf.summary.scalar("my_scalar",np.sin(step / 10), step=step)
+        data = (np.random.randn(100) + 2) * step / 100 # some random data
+        tf.summary.histogram("my_hist", data, buckets=50, step=step)
+        images = np.random.rand(2, 32, 32, 3) # random 32x32 RGB images
+        tf.summary.image("my_images", images * step / 1000, step=step)
+        texts = ["The step is " + str(step), "Its square is " + str(step**2)]
+        tf.summary.text("my_text", texts, step=step)
+        sine_wave = tf.math.sin(tf.range(12000) / 48000 * 2 * np.pi * step)
+        audio = tf.reshape(tf.cast(sine_wave, tf.float32), [1, -1, 1])
+        tf.summary.audio("my_audio", audio, sample_rate=48000, step=step)
+# =================================================================
+# 微调超参
+# 将Keras模型包装在模仿常规sklearn回归器的对象中
+def build_model(n_hidden=1, n_neurons=30, learning_rate=3e-3, input_shape=[8]):
+    model = keras.models.Sequential()
+    model.add(keras.layers.InputLayer(input_shape=input_shape))
+    for layer in range(n_hidden):
+        model.add(keras.layers.Dense(n_neurons, activation='relu'))
+    model.add(keras.layers.Dense(1))
+    optimizer = keras.optimizers.SGD(learning_rate=learning_rate)
+    model.compile(loss='mse', optimizer=optimizer)
+    return model
+# 基于该函数创建一个KerasRegressor
+keras_reg = keras.wrappers.scikit_learn.KerasRegressor(build_model)
+# keras_reg是一个keras模型的包装类对象，可以像使用sklearn回归器那样使用该对象
+keras_reg.fit(X_train, y_train, epochs=100,
+              validation_data=(X_valid, y_valid),
+              callbacks=[keras.callbacks.EarlyStopping(patience=10)])
+mse_test = keras_reg.score(X_test, y_test)
+y_pred = keras_reg.predict(X_new)
+# =================================
+# 使用随机搜索，训练数百个变体
+from scipy.stats import reciprocal
+from sklearn.model_selection import RandomizedSearchCV
+
+param_distribs = {
+    "n_hidden": [0, 1, 2, 3],
+    "n_neurons": np.arrange(1, 100),
+    "learning_rate": reciprocal(3e-4, 3e-2),
+}
+
+rnd_search_cv = RandomizedSearchCV(keras_reg, param_distribs, n_iter=10, cv=3)
+rnd_search_cv.fit(X_train, y_train, epochs=100,
+                  validation_data=(X_valid, y_valid),
+                  callbacks=[keras.callbacks.EarlyStopping(patience=10)])
