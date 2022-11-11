3399bb3a9ad8ab0190165898bfc1272b74e56777
dundundundun5 <1053450626@qq.com>
2022-11-11 16:52:08 +0800

    bilibili spider

--- web_crawler/bilibili/comments.py
+++ web_crawler/bilibili/comments.py
@@ -1,8 +1,8 @@
 from bilbili_spider import *
 
-bv = "BV11P4y1S7do"
+bv = "BV1Re4y1x72f"
 file = f'D:/temp_files/{bv}_comments.csv'
-get_comments_n_save(bv)
+# get_comments_n_save(bv)
 import pandas as pd
 a = pd.read_csv(file)
 print(type(a))
--- web_crawler/bilibili/replies.py
+++ web_crawler/bilibili/replies.py
@@ -1,7 +1,8 @@
 from bilbili_spider import *
 if __name__ == '__main__':
-    bv = "BV11P4y1S7do"
+    bv = "BV1Re4y1x72f"
     file = f'D:/temp_files/{bv}_replies.csv'
-    get_root_replies_n_save(bv)
+    # get_root_replies_n_save(bv)
     import pandas as pd
     a = pd.read_csv(file)
+    input("")
