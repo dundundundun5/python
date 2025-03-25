5ef34fe5385aaaea8fc37ebd525cc80ab9feda54
dundundundun5 <1053450626@qq.com>
2025-03-25 13:54:12 +0800

    无用文件删除+目录结构和名称变更

--- hands_on_machine_learning/end_to_end_project/check_data.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-housing: pd.DataFrame = pd.read_csv("D:\\python\\hands_on_machine_learning\\end_to_end_project\\datasets\\housing\\housing.csv")
-print(housing)
--- 计算机网络与仿真/scapy/sniffing_using_scapy.py
+++
@@ -1,9 +0,0 @@
-from scapy.all import sniff
-print(help(sniff))
-# timeout=在一定时间后停止嗅探
-# pkts_list = sniff(timeout=5)
-from scapy.all import wrpcap
-print(help(wrpcap))
-#  将嗅探到的所有包，存储为 NAME.pcap
-# wrpcap('scapy.pcap', pkts_list)
-quit()
